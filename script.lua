87d0244b4bb5d05e928331a9cf22e04f344cf796
Tazyyy._ <antoninoierna71@gmail.com>
2026-03-19 15:48:07 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e3eea5fa89d047dbadda7ffd1439a5f481f6946b0050156d9ead6ce6ab99ac5c/download"))()
